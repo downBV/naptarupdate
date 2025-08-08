87dbbe74c5d10923ad723208a562af67afd2b436
downBV <bvik159@gmail.com>
2025-08-08 02:57:09 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'muszakrend-cache-v3';
+const CACHE_NAME = 'muszakrend-cache-v4';
 const urlsToCache = [
   '/naptarupdate/',
   '/naptarupdate/index.html',
@@ -65,5 +65,6 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
 
 
